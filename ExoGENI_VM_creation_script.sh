ee49336efe32b0ca103574703d6791065800c56a
michelescarlato <michele.scarlato@gmail.com>
2020-12-06 00:53:07 +0900

    Update ExoGENI_VM_creation_script.sh

--- ExoGENI_VM_creation_script.sh
+++ ExoGENI_VM_creation_script.sh
@@ -10,3 +10,7 @@ sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 su - ${USER}
 id -nG
+#https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
